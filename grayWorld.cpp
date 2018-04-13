93f6ceaf3969677f044667ab5dfa37923ace6713
Jason.liu <liushaojie@zhiyuntcm.com>
2018-04-13 11:32:38 +0800

    add Lab mask to exclude too high and too low ab; restore grayWorld express to B+G+R/3;

--- grayWorld.cpp
+++ grayWorld.cpp
@@ -3,7 +3,22 @@
   
 using namespace cv;  
 using namespace std;
+
+AB_HIGH = 145;
+AB_LOW = 110;
   
+void showMeanLab(const Mat labMap)
+{
+    //mean Lab
+    double L, a, b;
+    vector<Mat> imageLab;
+    split(labMap, imageLab);
+    L = mean(imageLab[0])[0];
+    a = mean(imageLab[1])[0];
+    b = mean(imageLab[2])[0];
+    cout << "L,a,b:" << L << " " << a << " " << b << endl;
+}
+
 int main(int argc, char** argv)  
 {  
     char * filename = "test.jpg";
@@ -23,17 +38,39 @@ int main(int argc, char** argv)
     cvtColor(imageSource, labMap_ori, COLOR_BGR2Lab);    
     cvNamedWindow("Lab_ori", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
     imshow("Lab_ori", labMap_ori);
-    
-    //mean Lab
-    double L, a, b;
-    vector<Mat> imageLab_ori;  
-    split(labMap_ori, imageLab_ori);
-    L = mean(imageLab_ori[0])[0];
-    a = mean(imageLab_ori[1])[0];
-    b = mean(imageLab_ori[2])[0];
-    cout << "imageLab_ori L,a,b:" << L << " " << a << " " << b << endl;
+    showMeanLab(labMap_ori);
+    imwrite(("Lab_" + op_imagename).c_str(), labMap_ori);
 
-  
+    vector<Mat> imageLab;
+    split(labMap_ori, imageLab);
+    Mat lab_A, lab_B;
+    lab_A = imageLab[1];
+    lab_B = imageLab[2];
+    Mat lab_mask = Mat::zeros(lab_A.rows, lab_A.cols, CV_8U);
+        
+    
+    uchar* pxvec_a=lab_A.ptr<uchar>(0);
+    uchar* pxvec_b=lab_B.ptr<uchar>(0);
+    uchar* pxvec_m=lab_mask.ptr<uchar>(0);
+    //遍历访问Mat中各个像素值
+    for (int i = 0; i < lab_A.rows; i++)
+    {
+        pxvec_a = lab_A.ptr<uchar>(i);
+        pxvec_b = lab_B.ptr<uchar>(i);
+        pxvec_m = lab_mask.ptr<uchar>(i);
+        //三通道数据都在第一行依次排列，按照BGR顺序
+        for (int j = 0; j < lab_A.cols*lab_A.channels(); j++)
+        {
+            if(pxvec_a[j]>AB_HIGH || pxvec_b[j]>AB_HIGH || pxvec_a[j]<AB_LOW || pxvec_b[j]<AB_LOW)
+            {
+                continue;
+            }
+            pxvec_m[j] = 255; 
+        }
+    }
+    cvNamedWindow("lab_mask", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
+    imshow("lab_mask", lab_mask);
+
     //RGB三通道分离  
     split(imageSource, imageRGB);  
   
@@ -45,19 +82,26 @@ int main(int argc, char** argv)
 
     //求原始图像的RGB分量的均值  
     double R, G, B;  
-    B = mean(imageRGB[0])[0];  
-    G = mean(imageRGB[1])[0];  
-    R = mean(imageRGB[2])[0];  
+    B = mean(imageRGB[0], lab_mask)[0];  
+    G = mean(imageRGB[1], lab_mask)[0];  
+    R = mean(imageRGB[2], lab_mask)[0];  
+    //B = mean(imageRGB[0])[0];  
+    //G = mean(imageRGB[1])[0];  
+    //R = mean(imageRGB[2])[0];  
   
     //super para for value
-    double KValue = (SB+SG+SR)/(B+G+R);
+    //double KValue = (SB+SG+SR)/(B+G+R);
+    double KValue = 1;
     double FIX_KValue = 1.0;
 
     //需要调整的RGB分量的增益  
     double KR, KG, KB;  
-    KB = FIX_KValue * KValue * (R + G + B) * SB / ((SB + SG + SR) * B);  
-    KG = FIX_KValue * KValue * (R + G + B) * SG / ((SB + SG + SR) * G);  
-    KR = FIX_KValue * KValue * (R + G + B) * SR / ((SB + SG + SR) * R);  
+    //KB = FIX_KValue * KValue * (R + G + B) * SB / ((SB + SG + SR) * B);  
+    //KG = FIX_KValue * KValue * (R + G + B) * SG / ((SB + SG + SR) * G);  
+    //KR = FIX_KValue * KValue * (R + G + B) * SR / ((SB + SG + SR) * R);  
+    KB = FIX_KValue * KValue * (R + G + B) / (3 * B);  
+    KG = FIX_KValue * KValue * (R + G + B) / (3 * G);  
+    KR = FIX_KValue * KValue * (R + G + B) / (3 * R);  
   
     //调整RGB三个通道各自的值  
     imageRGB[0] = imageRGB[0] * KB;  
@@ -68,6 +112,7 @@ int main(int argc, char** argv)
     merge(imageRGB, imageSource);  
     cvNamedWindow("after", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
     imshow("after", imageSource);  
+    imwrite(op_imagename.c_str(), imageSource);
     cout << "before B,G,R:" << B << " " << G << " " << R << endl;
     cout << "KB,KG,kR:" << KB << " " << KG << " " << KR << endl;
 
@@ -81,39 +126,8 @@ int main(int argc, char** argv)
     //Lab
     Mat labMap = imageSource.clone();
     cvtColor(imageSource, labMap, COLOR_BGR2Lab);    
-    cvNamedWindow("Lab", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
-    imshow("Lab", labMap);
+    showMeanLab(labMap);
     
-    //mean Lab
-    vector<Mat> imageLab;  
-    split(labMap, imageLab);
-    L = mean(imageLab[0])[0];
-    a = mean(imageLab[1])[0];
-    b = mean(imageLab[2])[0];
-    cout << "after L,a,b:" << L << " " << a << " " << b << endl;
-    
-    //super paras for Lab
-    double SL, Sa, Sb;
-    SL = 117.565;
-    Sa = 128.386;
-    Sb = 129.879;
-
-    double KL = SL / L;
-    imageLab[0] = imageLab[0] * KL;
-    
-    merge(imageLab, labMap);
-
-    //求变换后图像Lab分量的均值  
-    L = mean(imageLab[0])[0];  
-    a = mean(imageLab[1])[0];  
-    b = mean(imageLab[2])[0];  
-    cout << "afterL L,a,b:" << L << " " << a << " " << b << endl;
-
-    cvtColor(labMap, imageSource, COLOR_Lab2BGR);    
-    cvNamedWindow("afterL", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
-    imshow("afterL", imageSource);
-
-    imwrite(op_imagename.c_str(), imageSource);
     
     imageSource.release();
     labMap_ori.release();
