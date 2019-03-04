b190c9ed7983ca2dfc12afd7f47b01397c3b0dad
jasonliu <liushaojie@zhiyuntcm.com>
2019-03-04 15:19:03 +0800

    add entropy calc

--- circle.cpp
+++ circle.cpp
@@ -18,20 +18,19 @@ int main(int argc, char** argv)
     
     int rows = imageSource.rows/16;
     int cols = imageSource.cols/16;
-    cout << imageSource.rows << imageSource.cols << endl;
-    cout << rows << cols << endl;
+    cout << imageSource.rows << " " << imageSource.cols << endl;
+    cout << rows << " " << cols << endl;
     Mat avgMeanMat(rows, cols, CV_32FC3, Scalar(0,0,0));
     Mat avgDevMat(rows, cols, CV_32FC3, Scalar(0,0,0));
    
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
-            //cout << imageSource.cols*j/cols << imageSource.rows*i/rows << imageSource.cols/cols << imageSource.rows/rows << endl;
             Mat srcROI = imageSource(Rect(imageSource.cols*j/cols,imageSource.rows*i/rows,imageSource.cols/cols,imageSource.rows/rows));
             stringstream ssBlock;
             ssBlock << "block" << i << j << ".jpg";
             string sBlockName = ssBlock.str();
             cout << "sBlockName:" << sBlockName << endl;
-            imwrite(sBlockName, srcROI);
+            //imwrite(sBlockName, srcROI);
 
             Scalar mean2;
             Scalar stddev;
@@ -45,6 +44,7 @@ int main(int argc, char** argv)
         }
     } 
 
+    // circle based on mean,dev
     Mat mulMat = avgMeanMat.mul(avgDevMat);
     vector<Mat> splitMulMatBGR;
     split(mulMat, splitMulMatBGR);
@@ -55,13 +55,51 @@ int main(int argc, char** argv)
     
     Scalar sumMeanMat = sum(mulMat);
     Scalar sumDevMat = sum(avgDevMat);
-    for(int i=0; i<3; i++){
-        cout << sumMeanMat[i]/sumDevMat[i] << endl;
+    double WB, WG, WR;
+    WB = sumMeanMat[0]/sumDevMat[0]; 
+    WG = sumMeanMat[1]/sumDevMat[1]; 
+    WR = sumMeanMat[2]/sumDevMat[2]; 
+    double Bg, Gg, Rg;
+    Bg = (WB + WG + WR)/(3*WB);
+    Gg = (WB + WG + WR)/(3*WG);
+    Rg = (WB + WG + WR)/(3*WR);
+    cout << "Bg,Gg,Rg:" << Bg << " " << Gg << " " << Rg << endl; 
+
+    //calc hist, discrete entropy
+    double hisB[256] = {0};
+    double hisG[256] = {0};
+    double hisR[256] = {0};
+    for(int i=0; i<imageSource.rows; i++){
+        for(int j=0; j<imageSource.cols; j++){
+            hisB[imageSource.at<Vec3b>(i,j)[0]] += 1;
+            hisG[imageSource.at<Vec3b>(i,j)[1]] += 1;
+            hisR[imageSource.at<Vec3b>(i,j)[2]] += 1;
+        }
     }
-       
+    for(int i=0; i<256; i++){
+        hisB[i] /= imageSource.rows*imageSource.cols;
+        hisG[i] /= imageSource.rows*imageSource.cols;
+        hisR[i] /= imageSource.rows*imageSource.cols;
+    }
+    double HB=0, HG=0, HR=0;
+    for(int i=0; i<256; i++){
+        if(hisB[i] != 0) HB += hisB[i] * (log(hisB[i])/log(2));
+        if(hisG[i] != 0) HG += hisG[i] * (log(hisG[i])/log(2));
+        if(hisR[i] != 0) HR += hisR[i] * (log(hisR[i])/log(2));
+    }
+    HB = -HB/8;
+    HG = -HG/8;
+    HR = -HR/8;
+    double kB=0, kG=0, kR=0;
+    kB = (Bg -1)*HB + 1;
+    kG = (Gg -1)*HG + 1;
+    kR = (Rg -1)*HR + 1;
+    cout << "H:" << HB << HG << HR << endl;
+    cout << "k:" << kB << kG << kR << endl;
     
+
     //super paras
-    double SR, SG, SB;  
+    double SR=0, SG=0, SB=0;  
     SB = 111.245;
     SG = 114.335;
     SR = 116.251;
@@ -69,9 +107,11 @@ int main(int argc, char** argv)
     //求原始图像的RGB分量的均值  
     double R, G, B;  
     vector<Mat> imageRGB;  
+    vector<Mat> imageRGB2;  
 
     //RGB三通道分离  
     split(imageSource, imageRGB);  
+    split(imageSource, imageRGB2);  
                 
     B = mean(imageRGB[0])[0];  
     G = mean(imageRGB[1])[0];  
@@ -88,10 +128,19 @@ int main(int argc, char** argv)
     imageRGB[1] = imageRGB[1] * KG;  
     imageRGB[2] = imageRGB[2] * KR;  
   
+    imageRGB2[0] = imageRGB2[0] * kB;  
+    imageRGB2[1] = imageRGB2[1] * kG;  
+    imageRGB2[2] = imageRGB2[2] * kR;  
+
     //RGB三通道图像合并  
     merge(imageRGB, imageSource);  
     cvNamedWindow("after", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
     imshow("after", imageSource);  
+
+    merge(imageRGB2, imageSource);  
+    cvNamedWindow("after2", CV_WINDOW_NORMAL| CV_WINDOW_KEEPRATIO| CV_GUI_EXPANDED);
+    imshow("after2", imageSource);  
+
     cout << "B,G,R:" << B << " " << G << " " << R << endl;
     cout << "KB,KG,kR:" << KB << " " << KG << " " << KR << endl;
     //求变换后图像RGB分量的均值  
